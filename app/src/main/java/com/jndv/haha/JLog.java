b0af7fcbcd97495f3c779b1fdd609a893d7c11d5
xhh <523991987@qq.com>
2019-01-26 17:24:49 +0800

    添加Log工具类

---
+++ app/src/main/java/com/jndv/haha/JLog.java
@@ -0,0 +1,11 @@
+package com.jndv.haha;
+
+import android.util.Log;
+
+public class JLog {
+    private static final String TAG = "JLog";
+
+    public static void test() {
+        Log.e(TAG, "test: ");
+    }
+}
