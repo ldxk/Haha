57340ec93f52767f3f1f14cbd3e9c5cf9eab5ea5
xhh <523991987@qq.com>
2019-01-26 17:22:41 +0800

    初次提交

--- app/src/main/java/com/jndv/haha/MainActivity.java
+++ app/src/main/java/com/jndv/haha/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
